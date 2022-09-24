7479204cccb6fea178577ffcd4c283bc2860d05d
Sirox <71220271+Sirox228@users.noreply.github.com>
2022-09-24 16:34:53 +0300

    Update FreeplayState.hx

--- source/states/FreeplayState.hx
+++ source/states/FreeplayState.hx
@@ -269,6 +269,9 @@ class FreeplayState extends MusicBeatState
 			trace(md);
 		 */
 
+                 #if android
+                 addVirtualPad(FULL, A_B);
+                 #end
 	}
 
 	public function addSongData(songData:EngineData.SongData){
