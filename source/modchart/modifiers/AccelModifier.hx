4398d829dd158d74a80fdb54d26371b1dc3af25e
Sirox <71220271+Sirox228@users.noreply.github.com>
2022-09-24 20:42:27 +0300

    Update AccelModifier.hx

--- source/modchart/modifiers/AccelModifier.hx
+++ source/modchart/modifiers/AccelModifier.hx
@@ -19,7 +19,7 @@ class AccelModifier extends Modifier { // this'll be boost in ModManager
     var effectHeight = 500;
 
     var yAdjust:Float = 0;
-    lime.app.Application.current.window.alert("the shit\ndata: " + data + "\nplayer: " + player, "a");
+    lime.app.Application.current.window.alert("the shit\ndata: " + data + "\nplayer: " + player + "\ngetMod('reverse'): " + Std.string(Type.getClass(getMod("reverse"))), "a");
     var reversePercent = getMod("reverse").getScrollReversePerc(data,player);
     lime.app.Application.current.window.alert("ok, it worked, wtf", "a");
     var mult = CoolUtil.scale(reversePercent,0,1,1,-1);
