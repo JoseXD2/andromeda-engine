2f9551c2ab9112e1f9b74111a2b680bd29ea7912
Sirox <71220271+Sirox228@users.noreply.github.com>
2022-09-25 10:26:37 +0300

    Update TitleState.hx

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -114,12 +114,6 @@ class TitleState extends MusicBeatState
 			logoBl.animation.addByPrefix('bump', 'logo bumpin', 24);
 			logoBl.animation.play('bump');
 			logoBl.updateHitbox();
-
-                        gfDance = new FlxSprite(FlxG.width * 0.4, FlxG.height * 0.07);
-		        gfDance.frames = Paths.getSparrowAtlas('gfDanceTitle');
-		        gfDance.animation.addByIndices('danceLeft', 'gfDance', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
-		        gfDance.animation.addByIndices('danceRight', 'gfDance', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
-		        gfDance.antialiasing = true;
 		}
 		else
 		{
@@ -160,6 +154,11 @@ class TitleState extends MusicBeatState
 
 		//i know its wasteful but im a lazy ass
 
+                gfDance = new FlxSprite(FlxG.width * 0.4, FlxG.height * 0.07);
+		gfDance.frames = Paths.getSparrowAtlas('gfDanceTitle');
+		gfDance.animation.addByIndices('danceLeft', 'gfDance', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
+		gfDance.animation.addByIndices('danceRight', 'gfDance', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
+		gfDance.antialiasing = true;
 		if(EngineData.options.oldTitle)
 		{
 			add(gfDance);
@@ -224,7 +223,7 @@ class TitleState extends MusicBeatState
 
 	function getIntroTextShit():Array<Array<String>>
 	{
-		var fullText:String = sys.io.File.getContent(Generic.returnPath() + Paths.txt('introText'));
+		var fullText:String = Assets.getText(Paths.txt('introText'));
 
 		var firstArray:Array<String> = fullText.split('\n');
 		var swagGoodArray:Array<Array<String>> = [];
@@ -362,12 +361,10 @@ class TitleState extends MusicBeatState
 		logoBl.animation.play('bump');
 		danceLeft = !danceLeft;
 
-                if (EngineData.options.oldTitle) {
 		        if (danceLeft)
 			        gfDance.animation.play('danceRight');
 		        else
 			        gfDance.animation.play('danceLeft');
-                }
 
 		FlxG.log.add(curBeat);
 
